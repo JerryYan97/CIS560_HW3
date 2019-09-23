fca852ad0616a955b7c32220fef02cdf32cfb7ce
JerryYan97 <yanjiarui97@126.com>
2019-09-23 13:33:26 -0400

    Original Project

---
+++ cis560_rasterizer_3d/rasterizer.cpp
@@ -0,0 +1,26 @@
+#include "rasterizer.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/string_cast.hpp>
+#include <iostream>
+
+Rasterizer::Rasterizer(const std::vector<Polygon>& polygons)
+    : m_polygons(polygons)
+{}
+
+QImage Rasterizer::RenderScene()
+{
+    QImage result(512, 512, QImage::Format_RGB32);
+    // Fill the image with black pixels.
+    // Note that qRgb creates a QColor,
+    // and takes in values [0, 255] rather than [0, 1].
+    result.fill(qRgb(0.f, 0.f, 0.f));
+    // TODO: Complete the various components of code that make up this function
+    // It should return the rasterized image of the current scene
+    return result;
+}
+
+void Rasterizer::ClearScene()
+{
+    m_polygons.clear();
+}
